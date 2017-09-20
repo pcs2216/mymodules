38e5f6ebf6fa01afad0f57e3881b079377039ff5
Pedro Coronel Santiago <coronelsantiagop@gmail.com>
2017-09-20 17:06:26 -0500

    [REF] crm_editado : ocultar workflow, cambio automatico de stage en vista kanban

--- crm_editado/models/crm_curso.py
+++ crm_editado/models/crm_curso.py
@@ -5,30 +5,16 @@ from odoo import api, fields, models
 class x_crm(models.Model):
     _inherit = 'crm.lead'
 
-    #x_status_number = fields.Integer(string='Actual', compute='_compute_sale2')
-
     @api.depends('order_ids')
-    def _compute_sale_amount_total(self):
-        print '************************* before'
-        print "Stage %s" % self.stage_id
-        res = super(x_crm, self)._compute_sale_amount_total()
-        self.sale_number = self.sale_number
-        if self.sale_number == 0:
-            self.write({'stage_id': 1})
+    @api.one
+    def _compute_sale_amount_total(self): # nombre de la funcion heredada
+        print '********GBM***************** before' # un print para que se muestre en la consola, Si se muestra entonces is esta compilando esta parte del codigo
+        super(x_crm, self)._compute_sale_amount_total() # llamamos a la función padre para que ejecute su código original
+        #agregamos nuestro código    
+        self.sale_number = self.sale_number #asignamos para evitar error de asignacióny poder hacer estavariable dentro de nuestro código    
+        if self.sale_number == 0:   
+            self.write({'stage_id': 1}) 
         else:
-            self.write({'stage_id': 4})
-        print "Stage %s" % self.stage_id
-        self.sale_number = self.sale_number 
-        #return res
-
-    #x_status_number = fields.Integer(string=u'Actual', compute=_compute_sale2)
-
-    """
-
-    
-    @api.onchange('sale_number')
-    def _change_status_(self):
-        if self.sale_number > 0:
-            self.stage_id = 4
-            print "***************Stage %s" % self.stage_id
-    """
+            self.write({'stage_id': 3})
+        self.sale_number = self.sale_number #volvemos a asignar para corregir el error que sale al comentar la línea "return res"
+        # return res
