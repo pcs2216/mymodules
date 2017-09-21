df30b5d90607c730e5d28cc470854782c41f64c7
Pedro Coronel Santiago <coronelsantiagop@gmail.com>
2017-09-21 16:48:32 -0500

    [NEW] crm_cmt : crear folder con los modulos descargados de lservidor

--- crm_editado/models/crm_curso.py
+++ crm_editado/models/crm_curso.py
@@ -7,14 +7,18 @@ class x_crm(models.Model):
 
     @api.depends('order_ids')
     @api.one
-    def _compute_sale_amount_total(self): # nombre de la funcion heredada
-        print '********GBM***************** before' # un print para que se muestre en la consola, Si se muestra entonces is esta compilando esta parte del codigo
-        super(x_crm, self)._compute_sale_amount_total() # llamamos a la función padre para que ejecute su código original
-        #agregamos nuestro código    
-        self.sale_number = self.sale_number #asignamos para evitar error de asignacióny poder hacer estavariable dentro de nuestro código    
-        if self.sale_number == 0:   
-            self.write({'stage_id': 1}) 
-        else:
+    def _compute_sale_amount_total(self):  # nombre de la funcion heredada
+        # un print para que se muestre en la consola, Si se muestra entonces is esta compilando esta parte del codigo
+        print '********GBM*****************'
+        # llamamos a la función padre para que ejecute su código original
+        super(x_crm, self)._compute_sale_amount_total()
+        # agregamos nuestro código
+        # asignamos para evitar error de asignacióny poder hacer estavariable dentro de nuestro código
+        self.sale_number = self.sale_number
+        if self.sale_number != 0 and (str(self.stage_id) == 'crm.stage(1,)'):
             self.write({'stage_id': 3})
-        self.sale_number = self.sale_number #volvemos a asignar para corregir el error que sale al comentar la línea "return res"
+        # else:
+            #self.write({'stage_id': 3})
+        # volvemos a asignar para corregir el error que sale al comentar la línea "return res"
+        self.sale_number = self.sale_number
         # return res
