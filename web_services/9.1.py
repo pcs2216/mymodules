a4e28999070f3fb397a87416b146b63d6c746c46
Pedro Coronel Santiago <coronelsantiagop@gmail.com>
2017-10-26 17:38:21 -0500

    Respaldo 26 oct

--- web_services/9.1.py
+++ web_services/9.1.py
@@ -3,8 +3,9 @@ import csv
 import functools
 import xmlrpclib
 HOST = '70.35.200.126'
+#HOST = '172.17.0.2'
 PORT = 8069
-DB = 'CMT_Dev'
+DB = 'CMT_1_0'
 USER = 'gbarrientos@soluciones4g.com'
 PASS = 'Happy2016*'
 ROOT = 'http://%s:%d/xmlrpc/' % (HOST, PORT)
@@ -36,7 +37,10 @@ print partner_template
 """
 #partner_id = call('res.partner', 'write',[25], partner_template)
 #print partner_id
-ids = call('crm.lead', 'search_read', [],['stage_id'])
+
+
+
+ids = call('res.partner', 'search_read', [('id','=','7134')],['name','total_invoiced','category_id'])
 #print "\n partner ids %s" % ids
 for x in ids:
     print x
